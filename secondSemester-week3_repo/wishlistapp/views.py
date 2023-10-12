a32656677254d51c1a98eb9dfa74e086f1e83d93
jivirus <bighead29@naver.com>
2023-10-12 19:37:45 +0900

    week3_1

---
+++ secondSemester-week3_repo/wishlistapp/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from wishlistapp.forms import BlogForm
+from .models import Blog, Wishlist
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user  # 현재 로그인된 사용자를 할당
+            post.save()
+            return redirect('read')
+    else:
+        form = BlogForm()
+    return render(request, 'create.html', {'form': form})
+
+def read(request):
+    blogs = Blog.objects
+    return render(request, 'read.html', {'blogs': blogs})
+
+def detail(request, id):
+    blog = get_object_or_404(Blog, id=id)
+    already_in_wishlist = False
+    if request.user.is_authenticated:
+        already_in_wishlist = Wishlist.objects.filter(user=request.user, post=blog).exists()
+    
+    return render(request, 'detail.html', {'blog': blog, 'already_in_wishlist': already_in_wishlist})
+
+def wishlist(request) :
+    user = request.user
+    wishlists = Wishlist.objects.filter(user=user)
+
+    return render(request, 'wishlist.html', {'wishlists': wishlists})
+
+def bloglist(request):  #과제
+    blogs = Blog.objects
+    return render(request, 'bloglist.html', {'blogs':blogs})
+
+@login_required    
+def add_to_wishlist(request, post_id):
+    post = get_object_or_404(Blog, pk=post_id)
+    already_in_wishlist = Wishlist.objects.filter(user=request.user, post=post).exists()
+    if not already_in_wishlist:
+        wishlist_item = Wishlist.objects.create(user=request.user, post=post)
+    return redirect('wishlist')
+
+
+@login_required
+def remove_from_wishlist(request, post_id):
+    post = get_object_or_404(Blog, pk=post_id)
+    wishlist_item = get_object_or_404(Wishlist, user=request.user, post=post)
+    wishlist_item.delete()
+    return redirect('wishlist')
---
+++ secondSemester-week3_repo/wishlistpj/urls.py
@@ -0,0 +1,23 @@
+from django import views
+from django.contrib import admin
+from django.urls import path, include
+import wishlistapp.views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', wishlistapp.views.index, name ='index'),
+    path('new/', wishlistapp.views.new, name='new'),
+    path('new/create/', wishlistapp.views.create, name='create'),
+    path('read/', wishlistapp.views.read, name = 'read',),
+    path('detail/<str:id>/', wishlistapp.views.detail, name = 'detail'),
+
+    path('wishlist', wishlistapp.views.wishlist, name = 'wishlist'), 
+    path('add_to_wishlist/<str:post_id>/', wishlistapp.views.add_to_wishlist, name = 'add_to_wishlist'),
+    path('remove_from_wishlist/<str:post_id>/', wishlistapp.views.remove_from_wishlist, name = 'remove_from_wishlist'),
+    path('bloglist', wishlistapp.views.bloglist, name = 'bloglist'), #과제
+
+    path('user/', include('userapp.urls')),
+
+
+]
